447a2618511fdc996ffca8b4e85f6d30409da27a
Web <webd75369@gmail.com>
2026-02-09 20:59:17 +0530

    created protected procedure

--- src/backend/init.ts
+++ src/backend/init.ts
@@ -1,15 +1,39 @@
-import { initTRPC } from "@trpc/server";
+import { auth } from "@/lib/auth/auth";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { headers } from "next/headers";
 import { cache } from "react";
 import superjson from "superjson";
 
 export const createTRPCContext = cache(async () => {
-  return { userId: "user_123" };
+  return {
+    auth: await auth.api.getSession({
+      headers: await headers(),
+    }),
+  };
 });
 
-const t = initTRPC.create({
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>;
+
+const t = initTRPC.context<Context>().create({
   transformer: superjson,
+});
+
+const isAuthed = t.middleware(({ next, ctx }) => {
+  if (!ctx.auth?.session) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "The user is not authenticated",
+    });
+  }
+  return next({
+    ctx: {
+      ...ctx,
+      user: ctx.auth.user,
+    },
+  });
 });
 
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = baseProcedure.use(isAuthed);
