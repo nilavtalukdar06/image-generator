4f03907c91a7999d03b59b3b9adbee6298bdb8db
Web <webd75369@gmail.com>
2026-02-09 20:32:36 +0530

    added react provider hoc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
+import { TRPCReactProvider } from "@/backend/client";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,10 +17,12 @@ export default function RootLayout({ children }: child) {
   return (
     <html lang="en">
       <body className={cn(inter.className, "antialiased")}>
-        <main className="w-full">
-          {children}
-          <Toaster />
-        </main>
+        <TRPCReactProvider>
+          <main className="w-full">
+            {children}
+            <Toaster />
+          </main>
+        </TRPCReactProvider>
       </body>
     </html>
   );
