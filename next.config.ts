af96bec7e02b57b7c861ec8cc7279344d66bdea2
Web <webd75369@gmail.com>
2026-02-11 02:03:47 +0530

    updated images view

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/modules/images/components/view-images.tsx
+++ src/modules/images/components/view-images.tsx
@@ -1,9 +1,45 @@
 "use client";
 
+import { useTRPC } from "@/backend/client";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import React from "react";
+
 export function ViewImages() {
+  const trpc = useTRPC();
+  const query = useSuspenseQuery(trpc.images.getImages.queryOptions());
   return (
-    <div className="w-full my-4">
-      <p className="text-muted-foreground font-light">My Images</p>
-    </div>
+    <React.Fragment>
+      {query.data?.length === 0 ? (
+        <div className="w-full my-4">
+          <p className="text-muted-foreground font-light">
+            You have not created any image
+          </p>
+        </div>
+      ) : (
+        <div className="w-full my-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 place-items-center gap-4">
+          {query.data?.map((item) => (
+            <div className="w-full h-62.5" key={item.id}>
+              {item.status === "generated" && (
+                <div className="w-full h-full rounded-md">
+                  <Image
+                    src={item.imageUrl!}
+                    height={250}
+                    width={250}
+                    alt="generated-image"
+                    loading="lazy"
+                    className="w-full h-full rounded-md"
+                  />
+                </div>
+              )}
+              {item.status === "in_progress" && (
+                <Skeleton className="h-full w-full rounded-md" />
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+    </React.Fragment>
   );
 }
