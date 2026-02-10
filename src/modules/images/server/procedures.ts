8670c49b99fe458559f7c81c99b449fa6d4bc4e7
Web <webd75369@gmail.com>
2026-02-10 21:09:52 +0530

    created procedures to create a image

---
+++ src/modules/images/server/procedures.ts
@@ -0,0 +1,30 @@
+import { createTRPCRouter, protectedProcedure } from "@/backend/init";
+import { inngest } from "@/inngest/client";
+import prisma from "@/lib/prisma";
+import { z } from "zod";
+
+export const imageRouter = createTRPCRouter({
+  createImage: protectedProcedure
+    .input(
+      z.object({
+        prompt: z.string().min(5, "prompt is too short"),
+      }),
+    )
+    .mutation(async (opts) => {
+      const result = await prisma.image.create({
+        data: {
+          prompt: opts.input.prompt,
+          status: "in_progress",
+          userId: opts.ctx.user.id,
+        },
+      });
+      await inngest.send({
+        name: "generate/image",
+        data: {
+          prompt: opts.input.prompt,
+          userId: opts.ctx.user.id,
+        },
+      });
+      return result;
+    }),
+});
