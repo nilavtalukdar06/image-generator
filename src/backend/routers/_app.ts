c4bacee5d9ab5d899ffd1f87ca355655a85228f4
Web <webd75369@gmail.com>
2026-02-10 23:21:03 +0530

    minor fix

--- src/backend/routers/_app.ts
+++ src/backend/routers/_app.ts
@@ -1,5 +1,8 @@
+import { imageRouter } from "@/modules/images/server/procedures";
 import { createTRPCRouter } from "../init";
 
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  images: imageRouter,
+});
 
 export type AppRouter = typeof appRouter;
