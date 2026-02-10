703887a0c5b8d8f93ce4f2bb62e85ceb008a14b6
Web <webd75369@gmail.com>
2026-02-10 23:27:54 +0530

    added trpc mutation

--- src/modules/images/components/create-image.tsx
+++ src/modules/images/components/create-image.tsx
@@ -23,6 +23,10 @@ import {
 } from "@/components/ui/field";
 import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
+import { useTRPC } from "@/backend/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { Spinner } from "@/components/ui/spinner";
 
 const formSchema = z.object({
   prompt: z
@@ -32,6 +36,7 @@ const formSchema = z.object({
 });
 
 export function CreateImage() {
+  const trpc = useTRPC();
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -40,11 +45,25 @@ export function CreateImage() {
     },
   });
 
+  const mutation = useMutation(
+    trpc.images.createImage.mutationOptions({
+      onSuccess: () => {
+        toast.success("Image generation has been started");
+        form.reset();
+        setIsOpen(false);
+      },
+      onError: (error) => {
+        toast.error(error.message || "Failed to send image generation request");
+      },
+    }),
+  );
+
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    mutation.mutate({ prompt: values.prompt });
   };
+
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+    <Dialog open={isOpen || mutation.isPending} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
         <Button
           variant="destructive"
@@ -96,6 +115,7 @@ export function CreateImage() {
         </form>
         <DialogFooter>
           <Button
+            disabled={mutation.isPending}
             type="button"
             variant="destructive"
             className="rounded-none shadow-none font-light"
@@ -104,12 +124,14 @@ export function CreateImage() {
             Cancel It
           </Button>
           <Button
+            disabled={mutation.isPending}
             form="image-generation"
             type="submit"
             variant="outline"
             className="rounded-none shadow-none border bg-sidebar font-light"
           >
-            Create Image
+            {mutation.isPending && <Spinner />}
+            {mutation.isPending ? "Creating..." : "Create Image"}
           </Button>
         </DialogFooter>
       </DialogContent>
